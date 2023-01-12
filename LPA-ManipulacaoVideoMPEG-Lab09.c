daff1b3f8bb90d159f0738e85e653195e3cb7be7
josemateusc <jose.cordova@icomp.ufam.edu.br>
2023-01-12 14:23:32 +0000

    update lab9

--- LPA-ManipulacaoVideoMPEG-Lab09.c
+++ LPA-ManipulacaoVideoMPEG-Lab09.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
+#include<stdlib.h>
 #include<math.h>
 
 void main(int argc, char **argv){
@@ -12,42 +13,44 @@ void main(int argc, char **argv){
     
     FILE *mpeg_file_out = fopen(filename,"wb");
 
-    long int buffer_size = *argv[2] * 1000000;
-    unsigned char buffer = malloc(buffer_size);
-    unsigned char byte[4];
+    long int buffer_size_max = *argv[2] * 1024 *1024;
+    long int buffer_size = 0;
+    char *buffer = malloc(buffer_size_max);
+    char byte[4];
 
-    fseek(mpeg_file_out, 0L, SEEK_END);
-    long int new_file_size = ftell(mpeg_file_out);
-    fseek(mpeg_file_out, 0L, SEEK_SET);
+    //fseek(mpeg_file_out, 0L, SEEK_END);
+    long int new_file_size = 0;//ftell(mpeg_file_out);
+    //fseek(mpeg_file_out, 0L, SEEK_SET);
 
 
     while(!feof(mpeg_file)){
 
-        fread(byte,sizeof(unsigned char),4,mpeg_file);
+        fread(byte,4,1,mpeg_file);
 
-        if (memcmp(byte,"\x00\x00\x01\xB3") != 0 && !feof(mpeg_file)){
-            buffer = byte[1];
-            //retorna 3 bytes?
+        if (memcmp(byte,"\x00\x00\x01\xB3",4) != 0 && !feof(mpeg_file)){
+            buffer[buffer_size] = byte[0];
+            buffer_size++;
+            fseek(mpeg_file,-3,SEEK_CUR);
             continue;
         }
         else{
-            if(new_file_size + sizeof(buffer) > buffer_size){
+            if(new_file_size + buffer_size > buffer_size_max){
                 fclose(mpeg_file_out);
                 i++;
                 sprintf(filename,"video_parte_%d.mpg",i);
                 printf("Criando arquivo %s ..\n",filename);
                 *mpeg_file_out = fopen(filename,"wb");
-                fseek(mpeg_file_out, 0L, SEEK_END);
-                new_file_size = ftell(mpeg_file_out);
-                fseek(mpeg_file_out, 0L, SEEK_SET);
+                //fseek(mpeg_file_out, 0L, SEEK_END);
+                //new_file_size = ftell(mpeg_file_out);
+                //fseek(mpeg_file_out, 0L, SEEK_SET);
 
-                fwrite(buffer, sizeof(buffer),1,mpeg_file_out);
+                fwrite(buffer, buffer_size,1,mpeg_file_out);
 
-                new_file_size = sizeof(buffer);
+                new_file_size = buffer_size;
             }
-            else if(new_file_size + sizeof(buffer) <= buffer_size){
-                fwrite(buffer, sizeof(buffer),1,mpeg_file_out);
-                new_file_size += sizeof(buffer);
+            else if(new_file_size + buffer_size <= buffer_size_max){
+                fwrite(buffer, buffer_size,1,mpeg_file_out);
+                new_file_size += buffer_size;
             }
             else if(feof(mpeg_file)){
                 fclose(mpeg_file_out);
@@ -55,10 +58,9 @@ void main(int argc, char **argv){
             }
             else{
                 memcpy(buffer,byte,4);
-                //Sete o tamanho do buffer para 4, de forma que o buffer comece já com o código Sequence lido.
+                buffer_size = 4;
             }
         }
     }
-    free(buffer);
     fclose(mpeg_file);
 }
